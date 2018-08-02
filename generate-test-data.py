c2c7d2f2523800a26fd4322468761ddc96789a8f
Ralph Gasser <rg@pontius.ch>
2018-08-02 11:12:25 +0200

    Updated the generate-test-data.py script

--- generate-test-data.py
+++ generate-test-data.py
@@ -1,53 +1,189 @@
 import argparse
 import os.path as ops
+import os
 import numpy as np
 import cv2
+import tensorflow as tf
+import sys
 
-from local_utils import data_utils
+from local_utils import establish_char_dict
+
+char_dict_path=ops.join(os.getcwd(), 'data/char_dict/char_dict.json')
+ord_map_dict_path=ops.join(os.getcwd(), 'data/char_dict/ord_map.json')
+__char_list = establish_char_dict.CharDictBuilder.read_char_dict(char_dict_path)
+__ord_map = establish_char_dict.CharDictBuilder.read_ord_map_dict(ord_map_dict_path)\
 
 
+def char_list(self):
+    """
+
+    :return:
+    """
+    return self.__char_list
+
+
+def int64_feature(value):
+    """
+        Wrapper for inserting int64 features into Example proto.
+    """
+    if not isinstance(value, list):
+        value = [value]
+    value_tmp = []
+    is_int = True
+    for val in value:
+        if not isinstance(val, int):
+            is_int = False
+            value_tmp.append(int(float(val)))
+    if is_int is False:
+        value = value_tmp
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def float_feature(value):
+    """
+        Wrapper for inserting float features into Example proto.
+    """
+    if not isinstance(value, list):
+        value = [value]
+    value_tmp = []
+    is_float = True
+    for val in value:
+        if not isinstance(val, int):
+            is_float = False
+            value_tmp.append(float(val))
+    if is_float is False:
+        value = value_tmp
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def bytes_feature(value):
+    """
+        Wrapper for inserting bytes features into Example proto.
+    """
+    if not isinstance(value, bytes):
+        if not isinstance(value, list):
+            value = value.encode('utf-8')
+        else:
+            value = [val.encode('utf-8') for val in value]
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def char_to_int(self, char):
+    """
+
+    :param char:
+    :return:
+    """
+    temp = ord(char)
+    # convert upper character into lower character
+    if 65 <= temp <= 90:
+        temp = temp + 32
+
+    for k, v in self.__ord_map.items():
+        if v == str(temp):
+            temp = int(k)
+            return temp
+    raise KeyError("Character {} missing in ord_map.json".format(char))
+
+    # TODO
+    # Here implement a double way dict or two dict to quickly map ord and it's corresponding index
+
+
+def int_to_char(self, number):
+    """
+
+    :param number:
+    :return:
+    """
+    if number == '1':
+        return '*'
+    if number == 1:
+        return '*'
+    else:
+        return self.__char_list[str(number)]
+
+
+def encode_labels(self, labels):
+    """
+        encode the labels for ctc loss
+    :param labels:
+    :return:
+    """
+    encoded_labeles = []
+    lengths = []
+    for label in labels:
+        encode_label = [self.char_to_int(char) for char in label]
+        encoded_labeles.append(encode_label)
+        lengths.append(len(label))
+    return encoded_labeles, lengths
+
+
+def sparse_tensor_to_str(self, spares_tensor: tf.SparseTensor):
+    """
+    :param spares_tensor:
+    :return: a str
+    """
+    indices = spares_tensor.indices
+    values = spares_tensor.values
+    values = np.array([self.__ord_map[str(tmp)] for tmp in values])
+    dense_shape = spares_tensor.dense_shape
+
+    number_lists = np.ones(dense_shape, dtype=values.dtype)
+    str_lists = []
+    res = []
+    for i, index in enumerate(indices):
+        number_lists[index[0], index[1]] = values[i]
+    for number_list in number_lists:
+        str_lists.append([self.int_to_char(val) for val in number_list])
+    for str_list in str_lists:
+        res.append(''.join(c for c in str_list if c != '*'))
+    return res
+
+
+#
+#
+#
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--root', type=str)
     parser.add_argument('--out', type=str)
     args = parser.parse_args()
-    feature_io = data_utils.TextFeatureIO()
+    modes = ['train', 'test', 'val']
 
-    array = []
-    with open(ops.join(args.root, 'lexicon.txt'), 'r') as lexicon:
-        array = np.array([tmp.strip().split() for tmp in lexicon.readlines()])
+    with open(ops.join(args.root, 'lexicon.txt'), 'r') as lex_file:
+        dictionary = np.array([tmp.strip().split() for tmp in lex_file.readlines()])
 
-    with open(ops.join(args.root, 'annotation_test.txt'), 'r') as training:
-        info = np.array([tmp.strip().split() for tmp in training.readlines()])
-        test_images_org = [cv2.imread(ops.join(args.root, tmp), cv2.IMREAD_COLOR) for tmp in info[:, 0]]
-        test_images = [bytes(list(np.reshape(tmp, [100 * 32 * 3]))) for tmp in np.array([cv2.resize(tmp, (100, 32)) for tmp in test_images_org])]
-        test_labels = np.array([array[tmp] for tmp in info[:, 1]])
-        test_imagenames = np.array([ops.basename(tmp) for tmp in info[:, 0]])
-
-        test_tfrecord_path = ops.join(args.out, 'test_features.tfrecords')
-        feature_io.writer.write_features(tfrecords_path=test_tfrecord_path, labels=test_labels, images=test_images, imagenames=test_imagenames)
-
-    with open(ops.join(args.root, 'annotation_val.txt'), 'r') as training:
-        info = np.array([tmp.strip().split() for tmp in training.readlines()])
-        val_images_org = [cv2.imread(ops.join(args.root, tmp), cv2.IMREAD_COLOR) for tmp in info[:, 0]]
-        val_images = [bytes(list(np.reshape(tmp, [100 * 32 * 3]))) for tmp in np.array([cv2.resize(tmp, (100, 32)) for tmp in val_images_org])]
-        val_labels = np.array([array[tmp] for tmp in info[:, 1]])
-        val_imagenames = np.array([ops.basename(tmp) for tmp in info[:, 0]])
-
-        test_tfrecord_path = ops.join(args.out, 'val_features.tfrecords')
-        feature_io.writer.write_features(tfrecords_path=test_tfrecord_path, labels=val_labels, images=val_images, imagenames=val_imagenames)
-
-    with open(ops.join(args.root, 'annotation_train.txt'), 'r') as training:
-        info = np.array([tmp.strip().split() for tmp in training.readlines()])
-        train_images_org = [cv2.imread(ops.join(args.root, tmp), cv2.IMREAD_COLOR) for tmp in info[:, 0]]
-        train_images = [bytes(list(np.reshape(tmp, [100 * 32 * 3]))) for tmp in np.array([cv2.resize(tmp, (100, 32)) for tmp in train_images_org])]
-        train_labels = np.array([array[tmp] for tmp in info[:, 1]])
-        train_imagenames = np.array([ops.basename(tmp) for tmp in info[:, 0]])
-
-        test_tfrecord_path = ops.join(args.out, 'train_features.tfrecords')
-        feature_io.writer.write_features(tfrecords_path=test_tfrecord_path, labels=train_labels, images=train_images, imagenames=train_imagenames)
+    for mode in modes:
+        test_tfrecord_path = ops.join(args.out, '{}_features.tfrecords'.format(mode))
+        loadandexport(args.root, 'annotation_{}.txt'.format(mode), test_tfrecord_path, dictionary)
 
 
+#
+#
+#
+def loadandexport(root, name, out, dictionary):
+    with open(ops.join(root, name), 'r') as data, tf.python_io.TFRecordWriter(out) as writer:
+        info = np.array([tmp.strip().split() for tmp in data.readlines()])
+        index = 0
+        for entry in info:
+            index = index + 1
+            image = cv2.imread(ops.join(root, entry[0]), cv2.IMREAD_COLOR)
+            if image is not None:
+                image_org = cv2.resize(image, (100, 32))
+                filename = ops.basename(entry[0])
+                features = tf.train.Features(feature={
+                    'labels': int64_feature(int(entry[1])),
+                    'images': bytes_feature(bytes(list(np.reshape(image_org, [100 * 32 * 3])))),
+                    'imagenames': bytes_feature(filename)
+                })
+                example = tf.train.Example(features=features)
+                writer.write(example.SerializeToString())
+            sys.stdout.write('\r>>Writing {:d}/{:d} {:s} tfrecords'.format(index + 1, len(info), filename))
+            sys.stdout.flush()
+        sys.stdout.write('\n')
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
